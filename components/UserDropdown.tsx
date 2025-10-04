aecd0b6125cad8533b4d60ee68af0af0d53ecdd7
Shreerama9 <srssbvs007@gmail.com>
2025-10-04 17:28:47 +0530

    feat: implement user avatar component and update user dropdown with avatar integration

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,5 +1,6 @@
-'use-client';
-import { useRouter } from "next/router";
+"use client";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -8,33 +9,64 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const UserDropdown = () => {
     const router = useRouter();
-    const  handleSingout = async () => {
-        router.push("sign-in")   
-    }
+    const handleSingout = async () => {
+    router.push("sign-in");
+    };
 
-    const user = {name:'gayathri', email : 'gayathri@smail.com'};
-
-
+    const user = { name: "gayathri", email: "gayathri@smail.com" };
 
     return (
     <DropdownMenu>
         <DropdownMenuTrigger asChild>
-            <Button variant='ghost' className='flex items-center gap-2 text-gray-400 hover:text-yellow-200'></Button>            
+        <Button
+            variant="ghost"
+            className="flex items-center gap-2 text-gray-400 hover:text-yellow-400"
+        >
+            <Avatar className="h-8 w-8">
+            <AvatarImage src="https://ghpage--bucolic-dango-777807.netlify.app/assets/Profile/RAMA.jpeg" />
+            <AvatarFallback className="text-sm font-bold bg-yellow-500 text-yellow-900"></AvatarFallback>
+            {user.name[0]}
+            </Avatar>
+
+            <div className="hidden md:flex flex-col items-start">
+                <span className="text-base font-medium text-gray-400">
+                {user.name}
+                </span>
+            </div>
+        </Button>
         </DropdownMenuTrigger>
-        
-        <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
+
+        <DropdownMenuContent className="text-gray-400 ">
+            <DropdownMenuLabel>
+                <div className="flex relative items-center gap-3 py-2">
+                <Avatar className="h-10 w-10">
+            <AvatarImage src="https://ghpage--bucolic-dango-777807.netlify.app/assets/Profile/RAMA.jpeg" />
+            <AvatarFallback className="text-sm font-bold bg-yellow-500 text-yellow-900"></AvatarFallback>
+            {user.name[0]}
+            </Avatar>
+            <div className="flex flex-col ">
+                <span className="text-base font-medium text-gray-400">
+                    {user.name}
+                </span>
+                <span className="text-sm text-gray-400">
+                    {user.email}
+                </span>
+            </div>
+
+            </div>
+
+
+
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator className="bg-gray-600"/>
+            
+        </DropdownMenuContent>
     </DropdownMenu>
-  );
+    );
 };
 
 export default UserDropdown;
